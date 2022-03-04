5b8c561ebcbc0f2f786999fa539c562072eaf655
Yasmani <yasmany.07@gmail.com>
2022-03-04 18:38:30 -0500

    feat: add layout and index

---
+++ utils/styles.js
@@ -0,0 +1,39 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: "#203040",
+    "& a": {
+      color: "#ffffff",
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: "bold",
+    fontSize: "1.5rem",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  main: {
+    minHeight: "80vh",
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: "center",
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  form: {
+    maxWidth: 800,
+    margin: "0 auto",
+  },
+  navbarButton: {
+    color: "#ffffff",
+    textTransform: "initial",
+  },
+});
+
+export default useStyles;
